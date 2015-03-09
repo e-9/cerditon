02eae406ede73bf965eb2a2b80b6a0ce7bfb4b65
e-9 <abraham@codefire.mx>
2015-03-08 20:32:51 -0600

    Podium completo

--- Cerdoton_Site/cerdoton_app/static/cerdoton_app/js/cerdoton.js
+++ Cerdoton_Site/cerdoton_app/static/cerdoton_app/js/cerdoton.js
@@ -1,5 +1,25 @@
 $( document ).ready(function() {
 
+
+  function podiumAnimate() {
+        $('.bronze .podium').animate({
+            "height": "62px"
+        }, 1500);
+        $('.gold .podium').animate({
+            "height": "165px"
+        }, 1500);
+        $('.silver .podium').animate({
+            "height": "106px"
+        }, 1500);
+        $('.competition-container .name').delay(1000).animate({
+            "opacity": "1"
+        }, 500);
+    }
+    podiumAnimate();
+
+
+    
+
   function compare(a,b) {
     if (a.label < b.label)
        return -1;
